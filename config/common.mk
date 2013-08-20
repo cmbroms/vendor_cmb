cedd233a04e0f5479e2b3ddbd7c6ceb89235337f
nycbjr <nycbjr@gmail.com>
2013-08-20 12:19:45 -0400

    we want our ramdisk insecure

--- config/common.mk
+++ config/common.mk
@@ -77,6 +77,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Enable ADB authentication
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+ADDITIONAL_DEFAULT_PROPERTIES += ro.secure=0
 endif
 
 # Copy over the changelog to the device
